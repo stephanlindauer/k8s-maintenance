847fbb55289e6ab065331d4846a12c0183e49232
stephan lindauer <stephanlindauer@posteo.de>
2019-10-29 12:31:46 +0100

    add reconnector script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+gcloud compute instances list --filter 'tags.items=stephanlindauer-k8s' --filter 'status=RUNNING' --format='json' | jq .
+gcloud compute addresses list --filter 'name=stephanlindauer-k8s-ingress' --format='json' | jq .
+
+instance_name=$(gcloud compute instances list --filter 'tags.items=stephanlindauer-k8s' --filter 'status=RUNNING' --format='json' | jq -r .[0].name)
+access_config_name=$(gcloud compute instances list --filter 'tags.items=stephanlindauer-k8s' --filter 'status=RUNNING' --format='json' | jq -r .[0].networkInterfaces[0].accessConfigs[0].name)
+address=$(gcloud compute addresses list --filter 'name=stephanlindauer-k8s-ingress'  --format="json" | jq -r .[0].address)
+
+echo $instance_name
+echo $access_config_name
+echo $address
+
+gcloud compute instances delete-access-config $instance_name --access-config-name="$access_config_name" --zone='europe-west3-a'
+gcloud compute instances add-access-config $instance_name --access-config-name="$access_config_name" --address="$address" --zone='europe-west3-a'
+
+sleep infinity
